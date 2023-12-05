c9e7e41958568cd193a9c7fadb7d32edbeb2c5c4
Shashank Naik <shashanknaik808@gmail.com>
2023-12-05 15:37:01 +0530

    Implemented a Button to open a centered Modal with text and a 'Click Me' button

--- mui-tut/src/Test.jsx
+++ mui-tut/src/Test.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 function Test() {
     return (
-        <div>Test</div>
+        <div>
+            <Button onClick={() => setOpen(true)}>Open Modal</Button>
+            <Modal open={open} onClose={() => setOpen(false)}>
+                <Box position="absolute" top="50%" left="50%">
+                    <Typography>It is a Modal</Typography>
+                    <Button variant='contained' onClick={() => setOpen(false)}>Click Me</Button>
+                </Box>
+            </Modal>
+        </div>
     )
 }
 
